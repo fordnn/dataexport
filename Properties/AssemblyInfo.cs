4909841682c7a4595ec862ff9bf07ce0013d5baf
forDNN <support@fordnn.com>
2017-10-19 20:53:36 -0400

    Migration from CodePlex to GitHub

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("forDNN.DataExporter")]
+[assembly: AssemblyDescription("This module allows you to export Data from the DotNetNuke database. Users interface allows to select source of data: Tables, Views or Custom Query. It supports Excel, XML and CSV formats for export.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("forDNN")]
+[assembly: AssemblyProduct("Data Exporter for DNN (former DotNetNuke)")]
+[assembly: AssemblyCopyright("(c) forDNN 2003-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: GuidAttribute("28fe883a-71b4-490c-9af5-fe4b6cd04150")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
